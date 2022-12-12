5e59c064153141645824b2b88a1c02cab6c7ba1c
manuelmenama <manuel.mena090589@gmail.com>
2022-12-12 19:21:44 +0100

    almost ended only pubblic

--- db.php
+++ db.php
@@ -16,6 +16,12 @@ $natural_super_ingredients = [
   new Ingredient("Polpa di Barbabietola essiccata")
 ];
 
-$natural_super = new Food(1, "Natural Super Premium", "Monge", new Category("Cane", "fa-solid fa-dog"), 38.90, true, "https://arcaplanet.vtexassets.com/arquivos/ids/255544/monge-adult-medium-pollo-12kg-600x600.jpg", 12, $natural_super_ingredients, "Pollo");
 
-var_dump($natural_super);
+$foods[] = new Food(1, "Natural Super Premium", "Monge", new Category("Cane", "fa-solid fa-dog"), 38.90, true, "https://arcaplanet.vtexassets.com/arquivos/ids/255544/monge-adult-medium-pollo-12kg-600x600.jpg", 12, $natural_super_ingredients, "Pollo");
+
+
+$toys[] = new Toy(2, "Feater Teaser", "KONG", new Category("Gatto", "fa-solid fa-cat"), 6.44, true, "https://arcaplanet.vtexassets.com/arquivos/ids/255925/Feather-Tease-002.jpg", "Contiene erba gatta", "Medium");
+
+
+$accessories[] = new Accessory(3, "Cappottino impermeabile nero", "LOVEDI", new Category("Cane", "fa-solid fa-dog"), 32.83, true, "https://arcaplanet.vtexassets.com/arquivos/ids/225402/lovedi-cappotto-imp-con-agnellino-nero-tg-36.jpg", "small", "Sintetico impermeabile");
+
--- index.php
+++ index.php
@@ -21,6 +21,96 @@ include_once __DIR__ . '/db.php';
 </head>
 <body>
 
+  <header class="bg-dark p-2">
+
+    <h1 class="text-light">Arca di planet</h1>
+
+  </header>
+
+  <main class="bg-light">
+
+    <div class="container">
+      <h3>Food</h3>
+      <div class="row">
+        <?php foreach($foods as $food): ?>
+          <div class="col-6">
+            <div class="card">
+
+              <img src="<?php echo $food->image_path ?>" class="card-img-top" alt="<?php echo $food->name ?>">
+              <div class="card-body">
+                <h5 class="card-title"><?php echo $food->name ?></h5>
+                <p class="card-text"><?php echo $food->brand ?></p>
+                <div>
+                  <i class="<?php echo $food->category->icon ?>"></i>
+                </div>
+                <p>Price: <?php echo number_format($food->price, 2, ",", ".") ?> € <i class="<?php echo ($food->is_aviable) ? ("fa-solid fa-check text-success") : ("fa-solid fa-x text-danger") ?>"></i></p>
+                <p><?php echo $food->weight ?> Kg</p>
+                <p>Ingredienti:</p>
+                <ul>
+                  <?php foreach($food->ingredients as $ingredient): ?>
+                  <li><?php echo $ingredient->name ?></li>
+                  <?php endforeach; ?>
+                </ul>
+              </div>
+
+            </div>
+          </div>
+        <?php endforeach; ?>
+
+      </div>
+    </div>
+
+    <div class="container">
+      <h3>Toy</h3>
+      <div class="row">
+      <?php foreach($toys as $toy): ?>
+          <div class="col-6">
+            <div class="card">
+
+              <img src="<?php echo $toy->image_path ?>" class="card-img-top" alt="<?php echo $toy->name ?>">
+              <div class="card-body">
+                <h5 class="card-title"><?php echo $toy->name ?></h5>
+                <p class="card-text"><?php echo $toy->brand ?></p>
+                <div>
+                  <i class="<?php echo $toy->category->icon ?>"></i>
+                </div>
+                <p>Price: <?php echo number_format($toy->price, 2, ",", ".") ?> € <i class="<?php echo ($toy->is_aviable) ? ("fa-solid fa-check text-success") : ("fa-solid fa-x text-danger") ?>"></i></p>
+                <p>Feature: <?php echo $toy->feature ?></p>
+                <p>Taglia: <?php echo $toy->size ?></p>
+              </div>
+
+            </div>
+          </div>
+        <?php endforeach; ?>
+      </div>
+    </div>
+
+    <div class="container">
+      <h3>Accessory</h3>
+      <div class="row">
+      <?php foreach($accessories as $accessory): ?>
+          <div class="col-6">
+            <div class="card">
+
+              <img src="<?php echo $accessory->image_path ?>" class="card-img-top" alt="<?php echo $accessory->name ?>">
+              <div class="card-body">
+                <h5 class="card-title"><?php echo $accessory->name ?></h5>
+                <p class="card-text"><?php echo $accessory->brand ?></p>
+                <div>
+                  <i class="<?php echo $accessory->category->icon ?>"></i>
+                </div>
+                <p>Price: <?php echo number_format($accessory->price, 2, ",", ".") ?> € <i class="<?php echo ($accessory->is_aviable) ? ("fa-solid fa-check text-success") : ("fa-solid fa-x text-danger") ?>"></i></p>
+                <p>Taglia: <?php echo $accessory->size ?></p>
+                <p>Materiale: <?php echo $accessory->material ?></p>
+              </div>
+
+            </div>
+          </div>
+        <?php endforeach; ?>
+      </div>
+    </div>
+
+  </main>
 
   
 </body>
